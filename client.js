a8a2e4ae2bddca0c6750a7ce1fb6e70f27b87e91
Taku <gcobs135102@gmail.com>
2016-10-17 01:37:51 +0800

    I think TA's code is something wrong

--- client.js
+++ client.js
@@ -1,19 +1,65 @@
+// for TCP socket
 const net = require('net');
-const fs = require('fs');
-
 const HOST = process.argv[2] || '140.113.194.88';
 const PORT = process.argv[3] || 30000;
+// generate rsa key
+const ursa = require('ursa');
+const crypto = require('crypto');
+const key = ursa.generatePrivateKey(1024, 65537);
+const myPrivkeypem = key.toPrivatePem();
+const myPubkeypem = key.toPublicPem();
+const myID = new Buffer('0216023');
+var TAPubkey = null;
+var encrypted = null;
+// state
+var state = 'handshake';
 
 const socket = net.connect(PORT, HOST, function() {
-  console.log('Socket connected');
-  socket.write('hello');
+    console.log('Socket connected');
+    // Handshake
+    var hello = 'hello';
+    //socket.write(hello.length.toString());
+    socket.write(hello);
 });
 
 socket.on('data', function(data) {
-    // since data is buffer
-    console.log(data.toString('utf8'));
+    switch(state) {
+        case 'handshake':
+        case 'end': 
+            console.log(data.toString('utf8'));
+            break;
+        case 'receivePub':
+            // OAEP
+            TAPubkey = data.toString('utf8');
+            console.log(TAPubkey);
+            encrypted = crypto.publicEncrypt(TAPubkey, myID);
+            console.log(encrypted);
+            break;
+        case 'receiveMagic':
+            break;
+        default:
+            break;
+    }
+    changeState(state);
 });
 
 socket.on('end', function() {
-    console.log('Socket closed');
+    socket.end();
+    console.log('Socket end');
 });
+
+var changeState = function(s) {
+    switch(s) {
+        case 'handshake':
+            state = 'receivePub'
+            break;
+        case 'receivePub':
+            state = 'receiveMagic'
+            break;
+        case 'receiveMagic':
+            state = 'end'
+            break;
+        default:
+            break;
+    }
+}
