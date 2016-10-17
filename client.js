925b9d01287a8e61ff8acfc2747f57e805346eaa
Taku <gcobs135102@gmail.com>
2016-10-17 21:12:19 +0800

    Booooooooom

--- client.js
+++ client.js
@@ -2,40 +2,54 @@
 const net = require('net');
 const HOST = process.argv[2] || '140.113.194.88';
 const PORT = process.argv[3] || 30000;
+// handshake
+const hello = Buffer.from('hello', 'ascii');
+const sizeOfHello = Buffer.from([hello.length]);
 // generate rsa key
 const ursa = require('ursa');
 const crypto = require('crypto');
 const key = ursa.generatePrivateKey(1024, 65537);
 const myPrivkeypem = key.toPrivatePem();
 const myPubkeypem = key.toPublicPem();
-const myID = new Buffer('0216023');
+const sizeOfMyPubkey = Buffer.from([myPubkeypem.length]);
+const myID = Buffer.from('0216023');
 var TAPubkey = null;
 var encrypted = null;
+var sizeOfEncrypted = null;
 // state
 var state = 'handshake';
 
 const socket = net.connect(PORT, HOST, function() {
     console.log('Socket connected');
     // Handshake
-    var hello = 'hello';
-    //socket.write(hello.length.toString());
+    //socket.write(sizeOfHello);
     socket.write(hello);
+    //socket.write(sizeOfHello, function(){socket.write(hello);});
 });
 
 socket.on('data', function(data) {
     switch(state) {
         case 'handshake':
         case 'end': 
-            console.log(data.toString('utf8'));
+            console.log(data.toString('ascii'));
             break;
         case 'receivePub':
-            // OAEP
-            TAPubkey = data.toString('utf8');
-            console.log(TAPubkey);
+            // toString since OAEP.  Decode from 4 for removing the size of msg.
+            TAPubkey = data.toString('ascii', 4);
+            console.log('Receive TA\'s public key:\n%s\nlength:%s', TAPubkey, data.toString('hex', 0, 4));
+            // Send my public key
+            socket.write(sizeOfMyPubkey);
+            socket.write(myPubkeypem);
+            console.log('Send my public key:\n%s\nlength:%s', myPubkeypem.toString('ascii'), sizeOfMyPubkey.toString('ascii'));
+            // Send myID encrypted
             encrypted = crypto.publicEncrypt(TAPubkey, myID);
-            console.log(encrypted);
+            sizeOfEncrypted = Buffer.from([encrypted.length]);
+            socket.write(sizeOfEncrypted);
+            socket.write(encrypted);
+            console.log('Send myID encrypted:\n%s\nlength:%s', encrypted.toString('ascii'), sizeOfEncrypted.toString('ascii'));
             break;
         case 'receiveMagic':
+            console.log('FK');
             break;
         default:
             break;
